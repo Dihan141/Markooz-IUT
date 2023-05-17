8069a1455a3ffa8b6166d1c54fc3c36a89ed1530
Dihan141 <rhidwanrashid@iut-dhaka.edu>
2023-05-17 16:26:38 +0600

    added conversation, message, updated app.js

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,8 @@ const product = require("./controller/product");
 const order = require("./controller/order");
 const payment = require("./controller/payment");
 const withdraw = require("./controller/withdraw");
+const conversation = require("./controller/conversation");
+const message = require("./controller/message");
 
 
 app.use("/api/v2/user", user);
@@ -42,6 +44,8 @@ app.use("/api/v2/product", product);
 app.use("/api/v2/order", order);
 app.use("/api/v2/payment", payment);
 app.use("/api/v2/withdraw", withdraw);
+app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
 
 
 // it's for ErrorHandling
