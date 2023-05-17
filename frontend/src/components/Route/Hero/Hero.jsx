cde538db4259373f53afe0c0d485b906934f8f73
51AN <sianashsad@iut-dhaka.edu>
2023-05-17 22:54:45 +0600

    edit in header, navbar and hero

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -8,20 +8,21 @@ const Hero = () => {
       className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
       style={{
         backgroundImage:
-          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+          "url(https://i.ibb.co/D1PL0xp/image-2.jpg)",
       }}
     >
       <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
         <h1
-          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#ffffff] font-[600] capitalize`}
         >
-          Best Collection for <br /> home Decoration
+          All of IUT Merchandizes<br /> in one place
         </h1>
-        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
-          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
-          quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
-          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#d1cfcf]">
+          Find all trending and exclusive merchandizes such as T-shirts, caps, keychains and <br />
+          many more. Markooz offers both sellers and buyers of IUT a central platform for <br/>
+          the commerce of IUTian products  
+          {/* quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
+          <br /> aliquam deserunt officia. Dolorum saepe nulla provident. */}
         </p>
         <Link to="/products" className="inline-block">
             <div className={`${styles.button} mt-5`}>
